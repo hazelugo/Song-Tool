e1c2c2e561e813c4855ad74babb7d05fb210c63c
Hector Lugo <haze.lugo@gmail.com>
2026-03-12 22:45:26 -0500

    revert

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
-import 'dotenv/config'
-import { defineConfig } from 'drizzle-kit'
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+// Explicitly load .env.local for local development
+config({ path: ".env.local" });
 
 export default defineConfig({
-  out: './drizzle',
-  schema: './src/db/schema.ts',
-  dialect: 'postgresql',
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-})
+});
